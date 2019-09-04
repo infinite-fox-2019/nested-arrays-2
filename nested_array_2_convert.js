860b43f7b2ecb36a76d6906682253ca2663c5a3b
Panji SN <panji1595@gmail.com>
2019-09-04 12:46:52 +0700

    Tugas nested array 2

--- nested_array_2_convert.js
+++ nested_array_2_convert.js
@@ -12,7 +12,17 @@ let roster = [
 // [[roster[0][0], roster[1][0]], [roster[0][1], roster[1][1]],...
 
 function convert_roster_format (nestedArray) {
-  // your convert code here
+  let arr = [];
+  let obj = {};
+
+  for (let i = 1;i < nestedArray.length;i++){
+    for(let j = 0; j <nestedArray[i].length;j++){
+      obj[nestedArray[0][j]] = nestedArray[i][j];
+    }
+    arr.push(obj);
+    obj = {};
+  }
+  return arr;
 }
 
 let object_roster = convert_roster_format(roster)
