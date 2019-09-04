916a8e09a3a6b2b99981fc2014c3d3833994c8fb
Satyo Wicaksana <satyowicaksana@gmail.com>
2019-09-04 15:50:18 +0700

    add toCamelCase()

--- nested_array_2_convert.js
+++ nested_array_2_convert.js
@@ -12,17 +12,36 @@ let roster = [
 // [[roster[0][0], roster[1][0]], [roster[0][1], roster[1][1]],...
 
 function convert_roster_format (nestedArray) {
-    let result = [];
-    for(let i = 1; i < nestedArray.length; i++) {
-        let obj = {};
-        for(let j = 0; j < nestedArray[i].length; j++) {
-            obj[nestedArray[0][j]] = nestedArray[i][j];
-        }
-        result.push(obj);
-    }
+  let result = [];
+  for(let i = 0; i < nestedArray[0].length; i++) {
+    nestedArray[0][i] = toCamelCase(nestedArray[0][i]);
+  }
 
-    console.log(result);
-    return result;
+  for(let i = 1; i < nestedArray.length; i++) {
+    let obj = {};
+    for(let j = 0; j < nestedArray[i].length; j++) {
+      obj[nestedArray[0][j]] = nestedArray[i][j];
+    }
+    result.push(obj);
+  }
+
+  return result;
+}
+
+function toCamelCase(str) {
+  let result = '';
+  let splitStr = str.split(' ');
+  for(let i = 0; i < splitStr.length; i++) {
+    let word = splitStr[i];
+    if(i === 0) {
+      word = word[0].toLowerCase() + word.substring(1);
+    } else {
+      word = word[0].toUpperCase() + word.substring(1);
+    }
+    result += word;
+  }
+
+  return result;
 }
 
 let object_roster = convert_roster_format(roster)
@@ -30,7 +49,7 @@ console.log(object_roster[2])
 
 // => { "Number": 31, "Name": "Harvey Kay", "Position": "Shooting Guard", "Points per Game": [0, 30, 16, 0, 25] }
 
-console.log(object_roster[0]['Name'] == 'Joe Schmo') // outputs true
+console.log(object_roster[0]['name'] == 'Joe Schmo') // outputs true
 
 module.exports = {
   convert_roster_format
